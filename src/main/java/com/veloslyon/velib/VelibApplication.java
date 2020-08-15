324b990c527afd58a94f5a54171958430dc5fc8d
Khalid Boulssane <khalid.boulssane@hotmail.com>
2020-08-15 16:10:16 +0200

    patch du problème de JSONOBJECT to JSONARRAY

--- src/main/java/com/veloslyon/velib/VelibApplication.java
+++ src/main/java/com/veloslyon/velib/VelibApplication.java
@@ -6,6 +6,8 @@ import org.json.simple.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Iterator;
+
 @SpringBootApplication
 public class VelibApplication {
 
@@ -15,6 +17,13 @@ public class VelibApplication {
 
 		System.out.println(objet.get(0));
 
+		Iterator i = objet.iterator();
+
+		while (i.hasNext()) {
+			JSONObject slide = (JSONObject) i.next();
+			String title = (String)slide.get("contract_name");
+			System.out.println(title);
+		}
 
 		SpringApplication.run(VelibApplication.class, args);
 	}
